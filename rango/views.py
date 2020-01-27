92665f218719b58942c0d13ad8d4982a0e91b408
Andrew McNair <1102317m@student.gla.ac.uk>
2020-01-27 17:43:46 +0000

    C4 tests fin

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href=\'/rango/about/\'>About</a>")
+    #context dictionary hopefully holds keys for the {{}} in your template
+    context_dict={'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    #render the appropriate template
+    return render(request,'rango/index.html',context=context_dict)
 # Create your views here.
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href=\'/rango/\'>Index</a>")
+    context_dict={'boldmessage':  'This tutorial has been put together by ACM.' }
+    #render the appropriate template
+    return render(request,'rango/about.html',context=context_dict)
